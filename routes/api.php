6052cfc0c04ee4b56b31039327528ed6f3f9c57c
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-06-04 00:35:08 -0300

    Created route to view all courses

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,4 +24,8 @@ Route::controller(StudentController::class)->group(function () {
     Route::get('/student', 'index');
     Route::post('/student', 'registerEnrollment');
     Route::get('/student/{id}', 'showCoursesByStudentId');
+});
+
+Route::controller(CourseController::class)->group(function () {
+    Route::get('/course', 'index');
 });
