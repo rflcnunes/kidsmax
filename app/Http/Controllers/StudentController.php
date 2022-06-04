8ecc4bda59bb1d6f78f1e5adc1f3c45e62e66beb
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-06-03 23:34:42 -0300

    Created registerEnrollment in StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterEnrollmentRequest;
 use App\Http\Responses\ApiResponse;
 use App\Services\StudentService;
 
@@ -24,6 +25,28 @@ class StudentController extends Controller
                 'All students',
                 $data,
                 200);
+        } catch (\Exception $e) {
+            return new ApiResponse(
+                false,
+                $e->getMessage(),
+                null,
+                500);
+        }
+    }
+
+    public function registerEnrollment(RegisterEnrollmentRequest $request)
+    {
+        try {
+            $id = $request->input('student_id');
+            $course_id = $request->input('course_id');
+
+            $data = $this->studentService->registerEnrollment($id, $course_id);
+
+            return new ApiResponse(
+                true,
+                'Enrollment registered successfully',
+                $data,
+                201);
         } catch (\Exception $e) {
             return new ApiResponse(
                 false,
