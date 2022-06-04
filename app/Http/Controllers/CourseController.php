deb6b53c41358788efac418833d31f8217499db7
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-06-04 01:53:40 -0300

    Created delete method in controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -62,4 +62,15 @@ class CourseController extends Controller
             return new ApiResponse(false, $e->getMessage(), null, 500);
         }
     }
+
+    public function delete($id)
+    {
+        try {
+            $data = $this->courseService->deleteCourse($id);
+
+            return new ApiResponse(true, 'Course deleted successfully', $data, 200);
+        } catch (\Exception $e) {
+            return new ApiResponse(false, $e->getMessage(), null, 500);
+        }
+    }
 }
