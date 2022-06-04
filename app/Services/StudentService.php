677d709a67e99e2a07edab175be3029f7c758db3
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-06-03 23:08:16 -0300

    Inserted StudentRepositoryInterface in StudentService

--- app/Services/StudentService.php
+++ app/Services/StudentService.php
@@ -2,7 +2,14 @@
 
 namespace App\Services;
 
+use App\Repositories\Contracts\StudentRepositoryInterface;
+
 class StudentService
 {
+    private $studentRepository;
 
+    public function __construct(StudentRepositoryInterface $studentRepository)
+    {
+        $this->studentRepository = $studentRepository;
+    }
 }
