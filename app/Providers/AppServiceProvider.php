ff0af31877c500e30892e08118d5bc069e184a6a
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-06-03 23:02:42 -0300

    Registered Student contract in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\StudentRepositoryInterface;
+use App\Repositories\Eloquent\StudentRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+          StudentRepositoryInterface::class,
+          StudentRepository::class
+        );
     }
 
     /**
